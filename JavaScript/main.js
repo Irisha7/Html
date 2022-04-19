5d61ec49fc240b5d0cf3ccaa1087805f836c57ca
Irisha7 <irakuhar1997@icloud.com>
2022-04-19 16:21:16 +0300

    newless

---
+++ JavaScript/main.js
@@ -0,0 +1,100 @@
+let a1 = 'hello';
+console.log(a1);
+alert ('a1');
+document.write ('hello');
+
+let a2 = 'owu';
+console.log(a2);
+
+let a3 = 'com';
+console.log(a3);
+
+let a4 = 'ua';
+console.log(a4);
+
+let a5 = '1';
+console.log(a5);
+
+let a6 = '10';
+console.log(a6);
+
+let a7 = '-999';
+console.log(a7);
+
+let a8 = '123';
+console.log(a8);
+
+let a9 = '3.14';
+console.log(a9);
+
+let a10 = '2.7';
+console.log (a10);
+
+let a11 = '16';
+console.log(a11);
+
+let a12 = 'true';
+console.log (a12);
+
+let a13 = 'false';
+console.log(a13);
+
+let book = {
+    title: 'Twilight.Eclipse',
+    pages: 455,
+    genre: 'drama'
+}
+console.log(book);
+
+// let books = {
+//     title: 'The chanel sisters',
+//     pages: 360,
+//     genre: 'novel',
+//     authors: [
+//         'Judithe Little',
+//         'Kathy Glynn',
+//         'Isabel Munoz'
+//     ]
+// }
+// console.log(books);
+
+// let firstName = 'Ira';
+// let middleName = 'Volodymyrivna';
+// let lastName = 'Kukhar';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+// console.log(person);
+
+
+// let fN = prompt('enter you first name');
+// let mN = prompt('enter you middle name');
+// let lN = prompt('enter you last name');
+// let person = `${fN} ${mN} ${lN}`;
+// console.log(person);
+//
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+let books = [
+    {title: 'The little Prince', pages:450, genre:'biografy', authors: ['Daniel Defoe','Roald Dahl','Joan Aiken']},
+    {title: 'Becoming', pages:345, genre:'thriller', authors: ['Daniel Defoe','Roald Dahl','Joan Aiken']},
+    {title: 'Dinosaur Sounds', pages:395, genre:'fairytale', authors: ['Daniel Defoe','Roald Dahl','Joan Aiken']}
+]
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);
+
+
+
+
+
+
+
+
+
+
+
