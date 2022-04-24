4ce45ea4ad9ec64bfd2cc5ca42b404c15a5ef9f6
Irisha7 <irakuhar1997@icloud.com>
2022-04-24 13:15:35 +0300

    newlessons

---
+++ JavaScript/mainX2.js
@@ -0,0 +1,81 @@
+// let x = +prompt("give me X");
+// if (x===0) {
+//     console.log('Virno');
+// } else {
+//     console.log('NeVirno');
+// }
+
+
+// let time = +prompt('what time it is?');
+// if (time >= 0 && time <= 15) {
+//     console.log('first quarter');
+// } else if (time > 15 && time <= 30) {
+//     console.log('second quarter');
+// } else if (time > 30 && time <= 45) {
+//     console.log('third quarter');
+// } else if (time > 45 && time < 60) {
+//     console.log('fourth quarter');
+// }
+
+
+// let day = 10;
+// if (day >= 1 && day <= 11) {
+//     console.log('decade 1');
+// } else if (day > 11 && day <=20) {
+//     console.log('decade 2');
+// } else if (day > 20 && day <= 31) {
+//     console.log('decade 3');
+// }
+
+// switch ( +prompt("enter day number")) {
+//     case 1:
+//         console.log('have breakfast');
+//         break;
+//     case 2:
+//         console.log('teaching');
+//         break;
+//     case 3:
+//         console.log('teaching');
+//         break;
+//     case 4:
+//         console.log('teaching');
+//         break;
+//     case 5:
+//         console.log('teaching');
+//         break;
+//     case 6:
+//         console.log('teaching');
+//         break;
+//     case 7:
+//         console.log('teaching');
+//         break;
+//     default:
+// }
+
+
+// let a = 1;
+// let b = 2;
+// if (a > b) {
+//     console.log (a);
+// } else if (b > a) {
+//     console.log (b);
+// } else if (a === b) {
+//     console.log ('equal')
+// }
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (a > b && a > c) {
+    console.log(a);
+} else if (b > c && b > a) {
+    console.log(b);
+} else if ( c > a && c > b) {
+    console.log(c);
+} else if (a === b && b === c) {
+    console.log('equal');
+}
+
+
